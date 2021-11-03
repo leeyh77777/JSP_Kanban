1f4c1007243f525f24626a8815a3fde24ee1b254
Unknown123465 <86813264+Unknown123465@users.noreply.github.com>
2021-11-03 14:10:21 +0900

    first commit

---
+++ main/webapp/resources/js/kanban.js
@@ -0,0 +1,7 @@
+/**
+	칸반보드 전용
+	
+ */
+window.addEventListener("DOMContentLoaded", function(){
+	const addWork = document.querySelector(".add_work");	
+}, false);
---
+++ main/webapp/resources/js/layer.js
@@ -0,0 +1,17 @@
+/**
+	레이어팝업
+	
+ */
+const layer = {
+	/**
+		팝업 열기
+		
+	 */
+	popup(url, width, height) {
+		
+	},
+	/** 팝업 닫기 */
+	close() {
+		
+	}
+}
